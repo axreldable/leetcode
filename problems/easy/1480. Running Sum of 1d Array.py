e497eeee5ced7da8e4e9a909a0453a9b29533c6e
Aleksei Starikov <alekseistarikov@cloud.upwork.com>
2022-08-26 20:56:00 +0400

    Initial commit

---
+++ problems/easy/1480. Running Sum of 1d Array.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        rez = [nums[0]]
+        for i in nums[1:]:
+            rez.append(rez[-1] + i)
+        return rez
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.runningSum([1, 2, 3, 4]))
+    print(s.runningSum([1, 1, 1, 1, 1]))
+    print(s.runningSum([3, 1, 2, 10, 1]))
---
+++ problems/medium/1481. Least Number of Unique Integers after K Removals.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        d = defaultdict(lambda: 0)
+
+        for i in arr:
+            d[i] += 1
+
+        l = list(d.values())
+        l.sort()
+
+        i = 0
+        leng = len(l)
+        while k >= 0 and i < leng:
+            k -= l[i]
+            i += 1
+
+        if k == 0:
+            rez = len(l[i:])
+        elif k < 0:
+            rez = len(l[i:]) + 1
+        else:
+            # k > 0
+            rez = 0
+
+        return rez
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.findLeastNumOfUniqueInts([5, 5, 4], 1))
+    print(s.findLeastNumOfUniqueInts([5, 5, 4], 25))
+    print(s.findLeastNumOfUniqueInts([4, 3, 1, 1, 3, 3, 2], 3))
