5ee34760e8d212bdceab624a5d00aefc7be42107
axreldable <aleksei.starikov.ax@gmail.com>
2022-09-04 12:26:53 +0400

    Add another easy task

--- problems/easy/121. Best Time to Buy and Sell Stock.py
+++ problems/easy/121. Best Time to Buy and Sell Stock.py
@@ -8,36 +8,50 @@ class Solution:
             return 0
 
         smin = prices[0]
-        min_index = 0
-        smax = prices[0]
-        max_index = 0
-
-        profit = 0
-
+        mprofit = 0
         for i, el in enumerate(prices):
             if el < smin:
-                if max_index > min_index:
-                    tmp = smax - smin
-                    if tmp > profit:
-                        profit = tmp
-
                 smin = el
-                min_index = i
-            else:
-                if max_index < min_index:
-                    if el > smin:
-                        smax = el
-                        max_index = i
-                else:
-                    if el > smax:
-                        smax = el
-                        max_index = i
+            elif el - smin > mprofit:
+                mprofit = el - smin
 
-        if max_index > min_index:
-            tmp = smax - smin
-            if tmp > profit:
-                profit = tmp
-        return profit
+        return mprofit
+
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     if len(prices) == 0:
+    #         return 0
+    #
+    #     smin = prices[0]
+    #     min_index = 0
+    #     smax = prices[0]
+    #     max_index = 0
+    #
+    #     profit = 0
+    #
+    #     for i, el in enumerate(prices):
+    #         if el < smin:
+    #             if max_index > min_index:
+    #                 tmp = smax - smin
+    #                 if tmp > profit:
+    #                     profit = tmp
+    #
+    #             smin = el
+    #             min_index = i
+    #         else:
+    #             if max_index < min_index:
+    #                 if el > smin:
+    #                     smax = el
+    #                     max_index = i
+    #             else:
+    #                 if el > smax:
+    #                     smax = el
+    #                     max_index = i
+    #
+    #     if max_index > min_index:
+    #         tmp = smax - smin
+    #         if tmp > profit:
+    #             profit = tmp
+    #     return profit
 
 
 if __name__ == "__main__":
